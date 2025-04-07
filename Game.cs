a19a712ea8b881c1d48dd70d8af76c7dacc1da66
YSbookcase <mokos6283@gmail.com>
2025-04-08 00:55:19 +0900

    [dosc] 개발기획 부분 작성
    조금은 두서 없지만 혼자서 브레인스토밍을 거치면서 생각나는대로 기능을 적었습니다.

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGameProject
+{
+    class Game
+    {
+        public static void Run()
+        {
+
+
+        }
+
+
+
+    }
+}

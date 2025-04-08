5d5bf7e531a369f2766d82d5b9474e73d9a3f58e
YSbookcase <mokos6283@gmail.com>
2025-04-08 11:58:25 +0900

    [Feat] 플레이어 및 이동
    플레이어 클래스와 이동 좌표를 통해서 필드에 이동 가능하도록 구현(맵 개발 필요)

---
+++ Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGameProject
+{
+    public class Player
+    {
+        public Vector2 position;
+
+
+
+        public void Print()
+        {
+            Console.SetCursorPosition(position.x, position.y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write('P');
+            Console.ResetColor();
+
+
+        }
+
+
+
+
+        public void Action(ConsoleKey input)
+        {
+            switch (input)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    Move(input);
+                    break;
+                    //case ConsoleKey.I:
+                    //    inventory.Open();
+                    //    break;
+            }
+        }
+
+        private void Move(ConsoleKey input)
+        {
+            Vector2 targetPos = position;
+
+            switch (input)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    targetPos.y--;
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    targetPos.y++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    targetPos.x--;
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    targetPos.x++;
+                    break;
+            }
+
+            position = targetPos;
+
+        }
+    }
+}

bcb6251a02c594329d31de4fe2b9912be0966677
YSbookcase <mokos6283@gmail.com>
2025-04-08 11:02:44 +0900

    [Feat] 제작 유틸 추가
    텍스트 문자 생성과 관련 기능

--- Extension.cs
+++ Extension.cs
@@ -1,30 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniGameProject
+﻿namespace MiniGameProject
 {
     public static class Extension
     {
 
     }
     public static class Utility
-        {
+    {
 
-            public static void PressAnyKey(string text)
-            {
-                Console.WriteLine(text);
-                Console.WriteLine("계속하려면 아무키나 누르세요....");
-                Console.ReadKey(true);
+        public static void PressAnyKey(string text)
+        {
+            Console.WriteLine(text);
+            Console.WriteLine("계속하려면 아무키나 누르세요....");
+            Console.ReadKey(true);
 
             while (Console.KeyAvailable)
             {
                 Console.ReadKey(intercept: true); // 소비만 하고 화면엔 출력 안 함
             }
+
         }
 
 
+        public static void textPrint(string text = "text를 입력해주세요.", int delay = 0, ConsoleColor coler = ConsoleColor.White)
+        {
+            Thread.Sleep(delay);
+            Console.ForegroundColor = coler;
+            Console.WriteLine(text);
+            Console.ResetColor();
+
+        }
+
+
+
     }
+
+
 }
+

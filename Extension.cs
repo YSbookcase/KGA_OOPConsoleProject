221f408ecb469507caca95bc324c4ea1d158f731
YSbookcase <mokos6283@gmail.com>
2025-04-09 11:18:49 +0900

    [Feat] Utility PressAnyKey 개선

--- Extension.cs
+++ Extension.cs
@@ -20,6 +20,19 @@
 
         }
 
+        public static void PressAnyKey()
+        {
+            
+            Console.WriteLine("계속하려면 아무키나 누르세요....");
+            Console.ReadKey(true);
+
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(intercept: true); // 소비만 하고 화면엔 출력 안 함
+            }
+
+        }
+
 
         public static void textPrint(string text = "text를 입력해주세요.", int delay = 0, ConsoleColor coler = ConsoleColor.White)
         {

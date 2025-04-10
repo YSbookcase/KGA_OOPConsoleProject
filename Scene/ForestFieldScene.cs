64e173d4b92833b2ad969321f53dcaf5e5cda15d
YSbookcase <mokos6283@gmail.com>
2025-04-10 12:53:40 +0900

    [Feat] 이벤트 전후 이동 가능 변경

--- Scene/ForestFieldScene.cs
+++ Scene/ForestFieldScene.cs
@@ -36,14 +36,21 @@ namespace MiniGameProject.Scene
 
             gameObjects = new List<GameObject>();
             gameObjects.Add(new Place(Game.Scenes.FieldNearHomeScene.ToString(), 'N', new Vector2(1, 1)));
-            gameObjects.Add(new Place(Game.Scenes.MazeScene.ToString(), 'Q', new Vector2(6, 3)));
 
+            if (Game.EventOn)
+            {
+                gameObjects.Add(new Place(Game.Scenes.MazeScene.ToString(), 'Q', new Vector2(6, 3)));
+            }
+            else
+            {
+                gameObjects.Add(new Place("", 'X', new Vector2(6, 3)));
+            }
             Game.Player.position.x = 1;
             Game.Player.position.y = 1;
 
 
 
-        } 
+        }
 
         public override void Enter()
         {
@@ -51,11 +58,30 @@ namespace MiniGameProject.Scene
             {
                 Game.Player.position = new Vector2(1, 1);
             }
-            else if (Game.prevSceneName == Game.Scenes.MazeScene.ToString())
+            else if (Game.prevSceneName == Game.Scenes.MazeScene.ToString() && Game.EventOn)
             {
                 Game.Player.position = new Vector2(6, 3);
             }
             Game.Player.map = map;
+
+
+            // MazeScene 관련 위치
+            Vector2 mazePortalPos = new Vector2(6, 3);
+
+            // 먼저 해당 위치 오브젝트 제거 (중복 방지)
+            gameObjects.RemoveAll(go => go.position.Equals(mazePortalPos));
+
+            // 조건에 따라 추가
+            if (Game.EventOn)
+            {
+                gameObjects.Add(new Place(Game.Scenes.MazeScene.ToString(), 'Q', mazePortalPos));
+            }
+            else
+            {
+                gameObjects.Add(new Obstacle('X', mazePortalPos, "아직은 지나갈 수 없는 듯 하다."));
+            }
+
+
         }
 
 
